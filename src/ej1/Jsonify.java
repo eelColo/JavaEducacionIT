50af1e4b3b2888b892eb9f4f5903ba98d207b430
Franco <francolens2002@gmail.com>
2023-07-21 22:59:48 -0300

    Añadi Jsonify, utilice Scanner con su funcion hasNext() para detectar si habia un valor siguiente y guardar en un array los args e imprimirlos

--- src/ej1/Jsonify.java
+++ src/ej1/Jsonify.java
@@ -1,10 +1,26 @@
 package ej1;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Jsonify {
-
+	
+	
+	
 	public static void main(String[] args) {
 		
-
+		
+		Scanner entrada = new Scanner(System.in);
+								
+		
+		if(entrada.hasNext()) {
+			String[] y = args;
+			System.out.println("{data:" + Arrays.toString(y) + "}");
+		}
+		
 	}
+
+	
 
 }
