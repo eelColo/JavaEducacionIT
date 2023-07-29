9fef4737370fd5ea872e580f3d7ec114503a7277
Franco <francolens2002@gmail.com>
2023-07-29 17:13:10 -0300

    Pasando a limpio ejercicio 1 de java Jsonify, tuve que quitar el if porque era incoherente simplemente asigne el args al array y tuve que modificar enteramente el resultado, sobreescribi el tostring para que, junto con StringBuilder fuera anidando sin borrar los valores previos el array de strings

--- src/ej1/Jsonify.java
+++ src/ej1/Jsonify.java
@@ -1,26 +1,50 @@
 package ej1;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
+import java.lang.reflect.Parameter;
+
+ 
 public class Jsonify {
 	
+	private String[] valores;
 	
+	//sobreescribi tostring con una clase llamada stringbuilder 
+	//permite anidar cada vez mas strings sin sobreescribirlo
+	@Override
+	public String toString() {
+		StringBuilder build = new StringBuilder();
+		String resultado = "";
+		build.append("{\"data\":[");
+		for(int i = 0; i < valores.length; i++) {
+			build.append("\"" + valores[i] + "\"");
+			if(i < valores.length -1) {
+				build.append(",");
+			}
+		}
+		build.append("]}");
+		resultado = build.toString();
+		return resultado;
+	}
+	
+	public Jsonify(String[] valores) {
+		this.valores = valores;
+	}
 	
 	public static void main(String[] args) {
 		
-		
-		Scanner entrada = new Scanner(System.in);
-								
-		
-		if(entrada.hasNext()) {
 			String[] y = args;
-			System.out.println("{data:" + Arrays.toString(y) + "}");
-		}
+		
+			Jsonify jsonify = new Jsonify(y);					
+			
+			System.out.println( jsonify.toString());
+		
 		
 	}
 
+	
+
+	
+	
+	
 	
 
 }
